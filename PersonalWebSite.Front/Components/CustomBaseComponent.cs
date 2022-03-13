1d0cecbc27217f79e26ee3f2bba0cfa6e100b100
julien gourdon <julien.gourdon@live.fr>
2022-03-13 10:41:42 +0100

    avancement map

---
+++ PersonalWebSite.Front/Components/CustomBaseComponent.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Components;
+using PersonalWebSite.Front.Services;
+
+namespace PersonalWebSite.Front.Components
+{
+    public class CustomBaseComponent : ComponentBase
+    {
+        [Inject]
+        public LanguageService LanguageService { get; set; }
+    }
+}
---
+++ PersonalWebSite.Front/wwwroot/js/site.js
@@ -0,0 +1,9 @@
+﻿function initSvg(pIdentifier) {
+    window.zoomTiger = svgPanZoom(pIdentifier, {
+        zoomEnabled: true,
+        controlIconsEnabled: true,
+        fit: true,
+        center: true,
+        // viewportSelector: document.getElementById('demo-tiger').querySelector('#g4') // this option will make library to misbehave. Viewport should have no transform attribute
+    });
+}
